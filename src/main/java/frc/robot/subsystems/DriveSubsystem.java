0af8a937c52aa0317ccbcdb1450bad2c31729c9d
Jinyuan Zhao <1zhaojin2@gmail.com>
2023-03-06 14:34:07 -0500

    Cleaned massive unused comment

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -39,26 +39,22 @@ public class DriveSubsystem extends SubsystemBase {
 
 
   
-/*   TalonSRX Left1 = new TalonSRX(RobotMap.Left1);
+  /*
+  TalonSRX Left1 = new TalonSRX(RobotMap.Left1);
   TalonSRX Left2 = new TalonSRX(RobotMap.Left2);
   TalonSRX Right1 = new TalonSRX(RobotMap.Right1);
-  TalonSRX Right2 = new TalonSRX(RobotMap.Right2); */
+  TalonSRX Right2 = new TalonSRX(RobotMap.Right2); 
+  */
 
   CANSparkMax Left1 = new CANSparkMax(RobotMap.Left1, MotorType.kBrushless);
   CANSparkMax Left2 = new CANSparkMax(RobotMap.Left2, MotorType.kBrushless);
   CANSparkMax Right1 = new CANSparkMax(RobotMap.Right1, MotorType.kBrushless);
   CANSparkMax Right2 = new CANSparkMax(RobotMap.Right2, MotorType.kBrushless);
-
-
-
-
-
   
   public DriveSubsystem(){
     headingPIDControl.enableContinuousInput(-180, 180);
     headingPIDControl.setTolerance(pidTolerance);
   }
-
   
   public void TankDrive (double left, double right) {
 
@@ -92,7 +88,8 @@ public class DriveSubsystem extends SubsystemBase {
 
     SmartDashboard.putNumber("Angle", absAngle);
 
-    System.out.println("pitch: " + pitch + "  roll: " + roll + "  yaw: " + yaw + " absAngle: " + absAngle + " output: " + output);
+    System.out.println("pitch: " + pitch + "  roll: " + roll + "  yaw: " + yaw + " absAngle: " + absAngle + " output: " + output + " is Auto Balance On?: " + autoBalanceBoolean);
+    
     
   }
 
@@ -104,50 +101,3 @@ public class DriveSubsystem extends SubsystemBase {
     autoBalanceBoolean = b;
   }
 }
-
-  // static final double kOffBalanceAngleThresholdDegrees = 10;
-  // static final double kOonBalanceAngleThresholdDegrees = 5;
-
-    
-  // public void AutoBalance() {
-
-  //   ahrs.reset();
-     
-  //   if (!autoBalanceXMode && (Math.abs(yawAngleDegrees) >= Math.abs(kOffBalanceAngleThresholdDegrees))) {
-  //       autoBalanceXMode = true;
-  //   } else if (autoBalanceXMode && (Math.abs(yawAngleDegrees) <= Math.abs(kOonBalanceAngleThresholdDegrees))) {
-  //       autoBalanceXMode = false;
-  //   }
-  //   // if (!autoBalanceYMode && (Math.abs(pitchAngleDegrees) >= Math.abs(kOffBalanceAngleThresholdDegrees))) {
-  //   //     autoBalanceYMode = true;
-  //   // } else if (autoBalanceYMode && (Math.abs(pitchAngleDegrees) <= Math.abs(kOonBalanceAngleThresholdDegrees))) {
-  //   //     autoBalanceYMode = false;
-  //   // }
-
-
-
-  //   // Control drive system automatically,
-  //   // driving in reverse direction of pitch/roll angle,
-  //   // with a magnitude based upon the angle
-
-  //   if (autoBalanceXMode) {
-  //       double yawAngleRadians = yawAngleDegrees * (Math.PI / 180.0);
-  //       xSpeed = Math.sin(yawAngleRadians) * -0.5;
-  //   }
-  //   // if (autoBalanceYMode) {
-  //   //     double rollAngleRadians = rollAngleDegrees * (Math.PI / 180.0);
-  //   //     ySpeed = Math.sin(rollAngleRadians) * -1;
-  //   // }
-
-  //   try {
-  //     Left1.set(xSpeed);
-  //     Left2.set(xSpeed);
-  //     Right1.set(xSpeed);
-  //     Right2.set(xSpeed);
-  //   } catch (RuntimeException ex) {
-  //       String err_string = "Drive system error:  " + ex.getMessage();
-  //       DriverStation.reportError(err_string, true);
-  //   }
-
-  //   System.out.println("xSpeed: " + xSpeed + " yaw: " + yawAngleDegrees);
-  // }
